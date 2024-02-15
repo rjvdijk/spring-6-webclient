9046c3d2b72bdbe206d299e3688c736c37ea9011
Rob van Dijk <rob@optimust.nl>
2024-02-15 20:29:00 +0100

    320. Get List as Java POJOs

--- src/main/java/guru/springframework/client/BeerClient.java
+++ src/main/java/guru/springframework/client/BeerClient.java
@@ -1,6 +1,7 @@
 package guru.springframework.client;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import guru.springframework.model.BeerDTO;
 import reactor.core.publisher.Flux;
 
 import java.util.Map;
@@ -12,4 +13,6 @@ public interface BeerClient {
     Flux<Map> listBeerMap();
 
     Flux<JsonNode> listBeersJsonNode();
+
+    Flux<BeerDTO> listBeerDtos();
 }
