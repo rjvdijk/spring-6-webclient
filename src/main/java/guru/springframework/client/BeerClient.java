22335bc9149fd5e8dbe59a870c75d45abf93d766
Rob van Dijk <rob@optimust.nl>
2024-02-16 09:41:06 +0100

    324. HTTP Post with WebClient

--- src/main/java/guru/springframework/client/BeerClient.java
+++ src/main/java/guru/springframework/client/BeerClient.java
@@ -20,4 +20,6 @@ public interface BeerClient {
     Mono<BeerDTO> getBeerById(String id);
 
     Flux<BeerDTO> getBeerByBeerStyle(String beerStyle);
+
+    Mono<BeerDTO> createBeer(BeerDTO beerDto);
 }
--- src/main/java/guru/springframework/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/client/BeerClientImpl.java
@@ -59,4 +59,16 @@ public class BeerClientImpl implements BeerClient {
                 .retrieve().bodyToFlux(BeerDTO.class);
     }
 
+    @Override
+    public Mono<BeerDTO> createBeer(BeerDTO beerDto) {
+        return webClient.post().uri(BEER_PATH)
+                .body(Mono.just(beerDto), BeerDTO.class)
+                .retrieve()
+                .toBodilessEntity()
+                .flatMap(voidResponseEntity -> Mono.just(voidResponseEntity
+                        .getHeaders().get("Location").get(0)))
+                .map(path -> path.split("/")[path.split("/").length - 1])
+                .flatMap(this::getBeerById);
+    }
+
 }
